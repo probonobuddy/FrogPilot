HeyLowOwO/WhoFP

Force Drive State

frogpilot/common/frogpilot_variables.py
@@ -280,6 +280,8 @@ class FrogPilotVariables:
       toggle.liveValid = False
 
     toggle.debug_mode = self.params.get_bool("DebugMode")
+    toggle.force_offroad = self.params_memory.get_bool("ForceOffroad")
+    toggle.force_onroad = self.params_memory.get_bool("ForceOnroad")
 
     toggle.is_metric = self.params.get_bool("IsMetric")
     distance_conversion = 1 if toggle.is_metric else CV.FOOT_TO_METER
@@ -450,7 +452,7 @@ class FrogPilotVariables:
     toggle.device_shutdown_time = DEVICE_SHUTDOWN_TIMES.get(self.get_value("DeviceShutdown", cast=int, condition=device_management))
     toggle.increase_thermal_limits = self.get_value("IncreaseThermalLimits", condition=device_management)
     toggle.low_voltage_shutdown = self.get_value("LowVoltageShutdown", cast=float, condition=device_management, min=VBATT_PAUSE_CHARGING, max=12.5)
-    toggle.no_logging = self.get_value("NoLogging", condition=device_management) and not self.vetting_branch
+    toggle.no_logging = self.get_value("NoLogging", condition=device_management) and not self.vetting_branch or toggle.force_onroad
     toggle.no_uploads = self.get_value("NoUploads", condition=device_management) and not self.vetting_branch
     toggle.no_onroad_uploads = self.get_value("DisableOnroadUploads", condition=toggle.no_uploads)
 
@@ -569,7 +571,7 @@ class FrogPilotVariables:
 
     screen_management = self.get_value("ScreenManagement")
     toggle.screen_brightness = max(self.get_value("ScreenBrightness", cast=float, condition=screen_management), 1)
-    toggle.screen_brightness_onroad = self.get_value("ScreenBrightnessOnroad", cast=float, condition=(screen_management))
+    toggle.screen_brightness_onroad = self.get_value("ScreenBrightnessOnroad", cast=float, condition=(screen_management and not toggle.force_onroad))
     toggle.screen_recorder = self.get_value("ScreenRecorder", condition=screen_management) or toggle.debug_mode
     toggle.screen_timeout = self.get_value("ScreenTimeout", cast=float, condition=screen_management)
     toggle.screen_timeout_onroad = self.get_value("ScreenTimeoutOnroad", cast=float, condition=screen_management)

frogpilot/ui/qt/offroad/utilities.cc
@@ -40,6 +40,34 @@ FrogPilotUtilitiesPanel::FrogPilotUtilitiesPanel(FrogPilotSettingsWindow *parent
   }
   addItem(flashPandaButton);
 
+  FrogPilotButtonsControl *forceStartedButton = new FrogPilotButtonsControl(tr("Force Drive State"), tr("<b>Manually set openpilot to be offroad or onroad.</b>"), "", {tr("OFFROAD"), tr("ONROAD"), tr("OFF")}, true);
+  QObject::connect(forceStartedButton, &FrogPilotButtonsControl::buttonClicked, [this](int id) {
+    if (id == 0) {
+      params_memory.putBool("ForceOffroad", true);
+      params_memory.putBool("ForceOnroad", false);
+
+      updateFrogPilotToggles();
+    } else if (id == 1) {
+      params.put("CarParams", params.get("CarParamsPersistent"));
+      params.put("FrogPilotCarParams", params.get("FrogPilotCarParamsPersistent"));
+
+      params_memory.putBool("ForceOffroad", false);
+      params_memory.putBool("ForceOnroad", true);
+
+      updateFrogPilotToggles();
+    } else if (id == 2) {
+      params_memory.putBool("ForceOffroad", false);
+      params_memory.putBool("ForceOnroad", false);
+
+      updateFrogPilotToggles();
+    }
+  });
+  forceStartedButton->setCheckedButton(2);
+  if (forceOpenDescriptions) {
+    forceStartedButton->showDescription();
+  }
+  addItem(forceStartedButton);
+
   ButtonControl *resetTogglesButton = new ButtonControl(tr("Reset Toggles to Default"), tr("RESET"), tr("<b>Reset all toggles to their default values.</b>"));
   QObject::connect(resetTogglesButton, &ButtonControl::clicked, [parent, resetTogglesButton, this]() {
     if (ConfirmationDialog::confirm(tr("Are you sure you want to reset all toggles to their default values?"), tr("Reset"), this)) {

probonobuddy/FrogPilot

frogpilot/common/frogpilot_variables.py
@@ -306,6 +306,8 @@ class FrogPilotVariables:
       toggle.liveValid = False
 
     toggle.debug_mode = self.params.get_bool("DebugMode")
+    toggle.force_offroad = self.params.get_bool("ForceOffroad")
+    toggle.force_onroad = self.params.get_bool("ForceOnroad")
 
     toggle.is_metric = self.params.get_bool("IsMetric")
     distance_conversion = 1 if toggle.is_metric else CV.FOOT_TO_METER
@@ -475,7 +477,7 @@ class FrogPilotVariables:
     toggle.device_shutdown_time = DEVICE_SHUTDOWN_TIMES.get(self.get_value("DeviceShutdown", cast=int, condition=device_management))
     toggle.increase_thermal_limits = self.get_value("IncreaseThermalLimits", condition=device_management)
     toggle.low_voltage_shutdown = self.get_value("LowVoltageShutdown", cast=float, condition=device_management, min=VBATT_PAUSE_CHARGING, max=12.5)
-    toggle.no_logging = self.get_value("NoLogging", condition=device_management and not self.vetting_branch)
+    toggle.no_logging = self.get_value("NoLogging", condition=device_management and not self.vetting_branch) or toggle.force_onroad
     toggle.no_uploads = self.get_value("NoUploads", condition=device_management and not self.vetting_branch)
     toggle.no_onroad_uploads = self.get_value("DisableOnroadUploads", condition=toggle.no_uploads)
 
@@ -592,7 +594,7 @@ class FrogPilotVariables:
 
     screen_management = self.get_value("ScreenManagement")
     toggle.screen_brightness = max(self.get_value("ScreenBrightness", cast=float, condition=screen_management), 1)
-    toggle.screen_brightness_onroad = self.get_value("ScreenBrightnessOnroad", cast=float, condition=(screen_management))
+    toggle.screen_brightness_onroad = self.get_value("ScreenBrightnessOnroad", cast=float, condition=(screen_management and not toggle.force_onroad))
     toggle.screen_recorder = self.get_value("ScreenRecorder", condition=screen_management) or toggle.debug_mode
     toggle.screen_timeout = self.get_value("ScreenTimeout", cast=float, condition=screen_management)
     toggle.screen_timeout_onroad = self.get_value("ScreenTimeoutOnroad", cast=float, condition=screen_management)

frogpilot/ui/qt/offroad/utilities.cc
@@ -40,6 +40,34 @@ FrogPilotUtilitiesPanel::FrogPilotUtilitiesPanel(FrogPilotSettingsWindow *parent
   }
   addItem(flashPandaButton);
 
+  FrogPilotButtonsControl *forceStartedButton = new FrogPilotButtonsControl(tr("Force Drive State"), tr("<b>Force openpilot to be offroad or onroad.</b>"), "", {tr("OFFROAD"), tr("ONROAD"), tr("OFF")}, true);
+  QObject::connect(forceStartedButton, &FrogPilotButtonsControl::buttonClicked, [this](int id) {
+    if (id == 0) {
+      params.putBool("ForceOffroad", true);
+      params.putBool("ForceOnroad", false);
+
+      updateFrogPilotToggles();
+    } else if (id == 1) {
+      params.put("CarParams", params.get("CarParamsPersistent"));
+      params.put("FrogPilotCarParams", params.get("FrogPilotCarParamsPersistent"));
+
+      params.putBool("ForceOffroad", false);
+      params.putBool("ForceOnroad", true);
+
+      updateFrogPilotToggles();
+    } else if (id == 2) {
+      params.putBool("ForceOffroad", false);
+      params.putBool("ForceOnroad", false);
+
+      updateFrogPilotToggles();
+    }
+  });
+  forceStartedButton->setCheckedButton(2);
+  if (forceOpenDescriptions) {
+    forceStartedButton->showDescription();
+  }
+  addItem(forceStartedButton);
+
   ButtonControl *resetTogglesButton = new ButtonControl(tr("Reset Toggles to Default"), tr("RESET"), tr("<b>Reset all toggles to their default values.</b>"));
   QObject::connect(resetTogglesButton, &ButtonControl::clicked, [parent, resetTogglesButton, this]() {
     if (ConfirmationDialog::confirm(tr("Are you sure you want to reset all toggles to their default values?"), tr("Reset"), this)) {